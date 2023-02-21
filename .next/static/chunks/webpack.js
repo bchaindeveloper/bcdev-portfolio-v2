e6d9056f6062a925d9111967b45afc16d8eb7b1f
bchaindeveloper <bcdev3@protonmail.com>
2023-02-21 13:49:47 -0500

    hero photo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf6839e5750c88f0"; }
+/******/ 		__webpack_require__.h = function() { return "d3eabb19867eaccd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => (
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
 
         <img
-          src="./earthnetwork.jpg"
+          src="./cp.jpg"
           alt="cover"
           className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
         />
