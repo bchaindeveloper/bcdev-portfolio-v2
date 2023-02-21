5c9795b25570a7e7dbb51b2e134afa80c6ea2ce8
bchaindeveloper <bcdev3@protonmail.com>
2023-02-21 13:46:13 -0500

    Working branch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cf6839e5750c88f0"; }
+/******/ 		__webpack_require__.h = function() { return "e7bb2ea2f8acc6ac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- sections/About.jsx
+++ sections/About.jsx
@@ -22,7 +22,7 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-softwhite">I develop web3 and blockchain  applications. My expertise is in engineering smart contracts and developing user interfaces.</span>
+        <span className="font-extrabold text-softwhite">I develop full-stack web3 blockchain applications.</span>
 
       </motion.p>
 
