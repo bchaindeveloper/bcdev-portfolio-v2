d01b2c320557d688786b010b0bc2f813f884556b
bchaindeveloper <bcdev3@protonmail.com>
2023-02-21 15:02:09 -0500

    Education

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88153032c064460b"; }
+/******/ 		__webpack_require__.h = function() { return "0db6347df42c8359"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- constants/index.js
+++ constants/index.js
@@ -35,22 +35,29 @@ export const startingFeatures = [
 
 export const newFeatures = [
   {
+    imgUrl: '/graphql.png',
+    title: 'GraphQL Blog',
+    subtitle:
+        'A GraphQL blog on Web3, Blockchain, Cryptocureency and development tools. This project uses Next.js, TailwindCSS, GraphQL and GraphCMS.',
+    link: 'https://bchaindeveloper.com/',
+  },
+  {
     imgUrl: '/Ethereum.png',
     title: 'Ether Crowdfund',
     subtitle:
         'A crowdfunding smart contract with a Vite frontend. This applicatoin allows users to create their own campaign as well as fund others. Funding is done by connecting your MetaMask wallet.',
     link: 'https://web3sire.com/',
   },
-  {
-    imgUrl: '/react-seeklogo.com.svg',
-    title: 'My Porfolio',
-    subtitle:
-        'I built two seperate porfolio websites utilizing different styles. My bchaindeveloper.com website uses Next.JS and has a different design.',
-    link: 'https://bchaindeveloper.com/',
-  },
 ];
 
 export const insights = [
+  {
+    imgUrl: 'https://static-assets.codecademy.com/components/curriculum/path/front-end-engineer-career-path/curriculum-card.svg',
+    title: 'CodeCademy, Front-End Engineer',
+    subtitle:
+        'Full Front-End course covering HTML, CSS, JavaScript, React and much more.',
+    link: 'https://join.codecademy.com/learn/paths/front-end-engineer-career-path/'
+  },
   {
     imgUrl: '/FCC.jpg',
     title: 'Solidity, Blockchain, FullStack Web3 JS Tutorial',
@@ -64,13 +71,6 @@ export const insights = [
     subtitle:
         'This course covered the core concepts related to blockchain and smart contracts.I used Web3.py and Solidity to create an array of blockchain projects',
     link: 'https://www.youtube.com/watch?v=M576WGiDBdQ&t=17s'
-  },
-  {
-    imgUrl: 'https://static-assets.codecademy.com/components/curriculum/path/front-end-engineer-career-path/curriculum-card.svg',
-    title: 'CodeCademy, Front-End Engineer',
-    subtitle:
-        'Full Front-End course covering HTML, CSS, JavaScript, React and much more.',
-    link: 'https://join.codecademy.com/learn/paths/front-end-engineer-career-path/'
   },
   
 ];
